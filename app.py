30e80fa8f3a407ae11ac4658a35feca3895d9dd5
Alireza <alireza.codes@gmail.com>
2026-07-21 10:04:13 +0200

    add initial Flask app structure and basic pages

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/categories")
+def categories():
+    return render_template("categories.html")
+
+
+@app.route("/quiz")
+def quiz():
+    return render_template("quiz.html")
+
+
+@app.route("/result")
+def result():
+    return render_template("result.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
